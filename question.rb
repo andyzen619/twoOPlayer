251c5a7862f6728bea109cf2c66ff0d3da3fc629
andyzen619 <andyzen619@gmail.com>
2019-12-09 17:59:05 +0000

    Implemented turn and question classes

---
+++ question.rb
@@ -0,0 +1,10 @@
+class Question
+
+  attr_accessor :question, :solution
+
+  def initialize(question, solution)
+    @question = name
+    @solution = solution
+  end
+
+end
--- turn.rb
+++ turn.rb
@@ -1,4 +1,10 @@
 class Turn 
 
-  
+  attr_accessor :turnNumber, :player, :question
+
+  def initialize(turnNumber, player, question)
+    @turnNumber = turnNumber
+    @player = player
+    @question = question
+  end
 end
