b16967b603cbc9b9caa1472c0127442d6f5c11e8
andyzen619 <andyzen619@gmail.com>
2019-12-09 17:47:40 +0000

    Implemented player class

--- player.rb
+++ player.rb
@@ -1,1 +1,24 @@
-Class player
+class Player
+  attr_accessor :name, :input, :lives
+
+  def initialize(name)
+    @name = name
+    @input = nil
+    @lives = 3
+  end
+
+  def loseLife()
+    self.lives -= 1
+  end
+
+  def getInput()
+    self.input = gets.chomp
+  end
+end
+
+p1 = Player.new("Andy")
+p1.loseLife()
+puts p1.lives
+p1.getInput()
+puts p1.input
+
